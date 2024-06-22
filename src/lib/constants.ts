16efd5ac5be367694e87f4f798a4f5b48515da8d
MarioProjects <maparla@proton.me>
2024-06-22 08:54:46 +0200

    first commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const LINKS = {
+  github: "https://github.com/MarioProjects",
+  linkedin: "https://www.linkedin.com/in/mario-parre%C3%B1o-lara-a33585125/",
+  aidventure: "https://aidventure.es/",
+  kaggle: "https://www.kaggle.com/maparla",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
---
+++ uno.config.ts
@@ -0,0 +1,71 @@
+// uno.config.ts
+import { defineConfig, presetUno, presetWebFonts } from "unocss";
+
+export default defineConfig({
+  content: {
+    filesystem: ["**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}"],
+  },
+  theme: {
+    boxShadow: {
+      custom: `2px 2px 0`,
+      "custom-hover": `1px 1px 0`,
+    },
+    fontFamily: {
+      sans: ["CabinetGrotesk", "Satoshi"],
+    },
+    gridTemplateRows: {
+      "auto-250": "repeat(auto-fill, 250px)",
+    },
+    gridTemplateColumns: {
+      "4-minmax": "repeat(4, minmax(150px, 1fr))",
+    },
+    colors: {
+      gray: {
+        50: "#FAFAFA",
+        100: "#F5F5F5",
+        200: "#E5E5E5",
+        300: "#D4D4D4",
+        400: "#A3A3A3",
+        500: "#737373",
+        600: "#525252",
+        700: "#404040",
+        800: "#262626",
+        900: "#171717",
+      },
+      darkslate: {
+        
+        50: "#f7f7f7",
+        100: "#e3e3e3",
+        200: "#c9c9c9",
+        300: "#6b6b6b",
+        400: "#383838",
+        500: "#171717" /* Exactly your example for the background */,
+        600: "#141414",
+        700: "#111111",
+        800: "#0e0e0e",
+        900: "#0b0b0b" /* Deeper and darker */,
+      },
+      primary: {
+        100: "#F9CDD3",
+        200: "#F3A3AA",
+        300: "#EC7981",
+        400: "#E64F59",
+        500: "#6699CC",
+        600: "#CF2F3D",
+        700: "#B82534",
+        800: "#A01B2B",
+        900: "#891321",
+      },
+    },
+  },
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "fontshare",
+      fonts: {
+        sans: ["Cabinet Grotesk", "Satoshi"],
+        serif: "Zodiak",
+      },
+    }),
+  ],
+});
